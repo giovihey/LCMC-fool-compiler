918584d13ac8b2886053471cc678660f48d43b89
Aldo Visconti <aldo.visconti@studio.unibo.it>
2026-02-11 21:39:46 +0100

    Bu Settete

--- src/compiler/Test.java
+++ src/compiler/Test.java
@@ -10,7 +10,7 @@ import svm.*;
 public class Test {
     public static void main(String[] args) throws Exception {
    			
-    	String fileName = "bankLoan.fool";
+    	String fileName = "bankloan.fool";
 
     	CharStream chars = CharStreams.fromFileName(fileName);
     	FOOLLexer lexer = new FOOLLexer(chars);
